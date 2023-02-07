307ee5c406fc6ce4272de10732515db7b2b53e07
Javier Chacana <javier.chacana@codurance.com>
2023-02-07 18:01:33 +0100

    REFACTOR: Code cleanup

--- java/src/test/java/com/codurance/shoppingcart/unitTests/CartRepositoryShould.java
+++ java/src/test/java/com/codurance/shoppingcart/unitTests/CartRepositoryShould.java
@@ -3,29 +3,46 @@ package com.codurance.shoppingcart.unitTests;
 import com.codurance.shoppingcart.CartRepository;
 import com.codurance.shoppingcart.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CartRepositoryShould {
+
+  public static final Product SOME_PRODUCT = new Product("Some Product", 1.00, 1, 1.79, 10, 2.17);
+  public static final Product OTHER_PRODUCT = new Product("Other Product", 2.00, 1, 1.79, 10, 3.17);
+  private CartRepository cartRepository;
+
+  @BeforeEach
+  void setUp() {
+    cartRepository = new CartRepository();
+  }
 
   @Test
   void addItemToProductsList() {
-    CartRepository cartRepository = new CartRepository();
 
-    Product product = new Product("Some Product", 1.00, 1, 1.79, 10, 2.17);
-    cartRepository.add(product);
+    cartRepository.add(SOME_PRODUCT);
 
-    Assertions.assertEquals( 1, cartRepository.byProductName("Some Product"));
+    assertEquals( 1, cartRepository.byProductName("Some Product"));
   }
-  
+
   @Test
   void addMultipleSameItemsToProductList() {
-    CartRepository cartRepository = new CartRepository();
 
-    Product product = new Product("Some Product", 1.00, 1, 1.79, 10, 2.17);
-    cartRepository.add(product);
-    cartRepository.add(product);
+    cartRepository.add(SOME_PRODUCT);
+    cartRepository.add(SOME_PRODUCT);
 
-    Assertions.assertEquals( 2, cartRepository.byProductName("Some Product"));
+    assertEquals( 2, cartRepository.byProductName("Some Product"));
+  }
+
+  @Test
+  void addMultipleDifferentItemsToProductList() {
+
+    cartRepository.add(SOME_PRODUCT);
+    cartRepository.add(OTHER_PRODUCT);
+
+    assertEquals( 1, cartRepository.byProductName("Some Product"));
   }
 }
