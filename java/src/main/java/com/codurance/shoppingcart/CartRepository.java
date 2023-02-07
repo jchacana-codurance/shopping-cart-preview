849068447039369abef9d31741da659cc5edb36e
Javier Chacana <javier.chacana@codurance.com>
2023-02-07 17:53:01 +0100

    GREEN: Added support for CartRepository

--- java/src/main/java/com/codurance/shoppingcart/CartRepository.java
+++ java/src/main/java/com/codurance/shoppingcart/CartRepository.java
@@ -1,7 +1,18 @@
 package com.codurance.shoppingcart;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartRepository {
+  List<Product> shoppingList = new ArrayList<>();
+
   public void add(Product product) {
-    throw new UnsupportedOperationException();
+    shoppingList.add(product);
+  }
+
+  public int byProductName(String some_product) {
+    return Math.toIntExact(shoppingList.stream()
+      .filter(product -> some_product.equals(product.name()))
+      .count());
   }
 }
--- java/src/test/java/com/codurance/shoppingcart/unitTests/CartRepositoryShould.java
+++ java/src/test/java/com/codurance/shoppingcart/unitTests/CartRepositoryShould.java
@@ -2,6 +2,8 @@ package com.codurance.shoppingcart.unitTests;
 
 import com.codurance.shoppingcart.CartRepository;
 import com.codurance.shoppingcart.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class CartRepositoryShould {
@@ -13,6 +15,18 @@ public class CartRepositoryShould {
     Product product = new Product("Some Product", 1.00, 1, 1.79, 10, 2.17);
     cartRepository.add(product);
 
+    Assertions.assertEquals( 1, cartRepository.byProductName("Some Product"));
+  }
 
+  @Disabled
+  @Test
+  void addMultipleSameItemsToProductList() {
+    CartRepository cartRepository = new CartRepository();
+
+    Product product = new Product("Some Product", 1.00, 1, 1.79, 10, 2.17);
+    cartRepository.add(product);
+    cartRepository.add(product);
+
+    Assertions.assertEquals( 2, cartRepository.byProductName("Some Product"));
   }
 }
